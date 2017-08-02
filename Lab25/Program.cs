1b6957c62cfea3c772907b9f1b74f5b852c4bff3
pflisch <priffrodrigues@gmail.com>
2017-08-02 13:47:14 -0400

    interfaces and classes added

---
+++ Lab25/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hand hand = new Hand(); // new empty hand contianer
+
+            Deck deck = new Deck();
+
+            Console.WriteLine(deck.ToString());
+            Console.WriteLine();
+            deck.Shuffle();
+            Console.WriteLine(deck.ToString());
+            Console.WriteLine();
+
+            hand.Add(deck.Deal1()); // deal one card from the top of the deck
+            Console.WriteLine(hand.ToString());
+            Console.WriteLine();
+
+            hand.Add(deck.Deal1()); // deal one card from the top of the deck
+            Console.WriteLine(hand.ToString());
+
+            hand = deck.Deal5(); // Created a new Hand object and assigns it to hand
+            Console.WriteLine(hand.ToString());
+
+            hand.HighestCards();
+
+        }
+    }
+}
